959ef4467b9cd13dcf1298e3b7c264df353810b5
chloeRgit <chloe.rondeau2020@campus-eni.fr>
2021-11-09 17:46:07 +0100

    squelette pour le tableau des sortie

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,17 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main")
      */
-    public function index(): Response
+    public function index(SortieRepository $repo ): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $sorties=$repo->findAll();
+        $user=$this->getUser();
+
+
+
         return $this->render('main/index.html.twig', [
+            'sorties'=>$sorties,
+            'user'=>$user,
             'controller_name' => 'MainController',
         ]);
     }
