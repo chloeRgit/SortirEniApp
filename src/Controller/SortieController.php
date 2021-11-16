432991579fab34af2604dc188e2cfc2c7762bc6d
Amandine <amandine.gagnon2020@campus-eni.fr>
2021-11-16 10:53:32 +0100

    CREATION DES SORTIES V1 terminé

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -55,79 +55,100 @@ class SortieController extends AbstractController
     /**
      * @Route("create_sortie", name="app_sortie")
      */
-    public function creationSortie( Request $request,EtatRepository $etatRepository, SortieRepository $repo, LieuRepository $lieuRepository, SiteRepository $siteRepository, VilleRepository $villeRepository): Response
+    public function creationSortie(Request $request, EtatRepository $etatRepository, SortieRepository $repo, LieuRepository $lieuRepository, SiteRepository $siteRepository, VilleRepository $villeRepository): Response
     {
         $sortie = new Sortie();
         $rue = null;
         $cp = null;
         $lieuChoisi = null;
 
-        $lieu = $lieuRepository->findAll();
-        $ville = $villeRepository->findAll();
+        $lieuCat = $lieuRepository->findAll();
+        $villeRepo = $villeRepository->findAll();
 
         $organisteur = $this->getUser();
 
         $formSortie = $this->createForm(CreationSortieType::class, $sortie);
         $formSortie->handleRequest($request);
 
+        if (isset($_POST['action'])) {
+            if ($request->request->get('titre-sortie') != null){
+                $sortie->setNom($request->request->get('titre-sortie')) ;
+            }
 
-        if ($formSortie->isSubmitted() && $formSortie->isValid()){
+            if ($request->request->get('date-h-sortie') != null){
+                $timeSortie = new \DateTime($request->request->get('date-h-sortie'));
+                $timeSortie->format('Y-m-d H:i:s');
+                $sortie->setDateHeureDebut($timeSortie);
+            }
+
+            if ($request->request->get('date-l-sortie') != null){
+                $timeInscription = new \DateTime($request->request->get('date-l-sortie'));
+                $timeInscription->format('Y-m-d H:i:s');
+                $sortie->setDateLimiteInscription($timeInscription) ;
+            }
+
+            if ($request->request->get('nbParticipant-sortie') != null){
+                $sortie->setNbInscriptionsMax($request->request->get('nbParticipant-sortie')) ;
+            }
+
+            if ($request->request->get('duree-sortie') != null){
+                $sortie->setDuree($request->request->get('duree-sortie')) ;
+            }
+
+
+                $sortie->setInfosSortie($request->request->get('desc-sortie')) ;
+
+
+            if ($request->request->get('lieu-select') != null){
+                $idLieu = $request->request->get('lieu-select');
+                $lieuChoisi = $lieuRepository->findOneBy([
+                        "id" => $idLieu
+                    ]);
+                $sortie->setLieu($lieuChoisi);
+            }
+
+
             $sortie->setOrganisateur($organisteur);
+            $sortie->setSite($organisteur->getSite());
 
-            /*if (isset($_POST['1'])) {
-
-                $etat = $etatRepository->find(1);
-                console.log($etat);
-
-            } elseif (isset($_POST['2'])) {
-
-                $etat = $etatRepository->find(2);
-
-            } else {
-
-            }*/
-            $etat=$etatRepository->find(1);
-
+            $etat = $etatRepository->find($_POST['action']);
             $sortie->setEtat($etat);
-            dd($sortie);
+
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($sortie);
             $entityManager->flush();
 
             return $this->redirectToRoute('main');
+        } else {
+
         }
+
 
         return $this->render('main/creationsortie.html.twig', [
             'sortie' => $sortie,
-            'organisateur'=>$organisteur,
-            'lieux' => $lieu,
-            'ville' => $ville,
+            'organisateur' => $organisteur,
+            'lieux' => $lieuCat,
+            'ville' => $villeRepo,
             'rue' => $rue,
             'cp' => $cp,
             'siteOrganisateur' => $organisteur->getSite()->getNom(),
             'formSortie' => $formSortie->createView(),
         ]);
     }
+
     /**
      * @Route("/api/select_lieu/{id}", name="api_ville" ,methods={"GET"})
      */
-    public function selectLieu(LieuRepository $lieuRepo, VilleRepository $repo, $id): Response{
-        $ville = $repo->findOneBy(
+    public function selectLieu(LieuRepository $lieuRepo, VilleRepository $villeRepository, $id): Response
+    {
+        $ville = $villeRepository->findOneBy(
             [
-                "nom"=>$id
+                "id" => $id
             ]
         );
 
         $lieux = $lieuRepo->LieuFiltre($ville);
-        /*
-        $id_ville = $ville->getId();
-        $lieux = $lieuRepo->findBy(
-            [
-                "ville"=>$id_ville
-            ]
-        );
-        */
 
         return $this->json($lieux);
     }
@@ -135,18 +156,19 @@ class SortieController extends AbstractController
     /**
      * @Route("/api/info_lieu/{id}", name="api_lieux" ,methods={"GET"})
      */
-    public function infoLieu(LieuRepository $lieuRepo, VilleRepository $repo, $id): Response{
+    public function infoLieu(LieuRepository $lieuRepo, VilleRepository $repo, $id): Response
+    {
         $lieu = $lieuRepo->findOneBy(
             [
-                "id"=>$id
+                "id" => $id
             ]
         );
 
         $tabLieu = array();
-            $tabLieu[0] = $lieu->getRue();
-            $tabLieu[1] = $lieu->getVille()->getCodePostal();
-            $tabLieu[2] = $lieu->getLatitude();
-            $tabLieu[3] = $lieu->getLongitude();
+        $tabLieu[0] = $lieu->getRue();
+        $tabLieu[1] = $lieu->getVille()->getCodePostal();
+        $tabLieu[2] = $lieu->getLatitude();
+        $tabLieu[3] = $lieu->getLongitude();
 
         //dd($tabLieu);
 
