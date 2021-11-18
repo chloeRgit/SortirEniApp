b4df5b2d62e86c5589bc58cec2a9fe4d3c086b7e
Sandy Guerin <sandy.guerin2019@campus-eni.fr>
2021-11-18 14:13:41 +0100

    Creation sortie: ajout pour lieu et ville suite

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -133,7 +133,7 @@ class SortieController extends AbstractController
     /**
      * @Route("create_lieu", name="app_lieu")
      */
-    public function creationLieu(Request $request, VilleRepository $villeRepository): Response
+    public function creationLieu(Request $request, LieuRepository $lieuRepository, VilleRepository $villeRepository): Response
     {
         $lieu = new Lieu();
         $rue = null;
@@ -167,7 +167,6 @@ class SortieController extends AbstractController
         } else {
 
         }
-
 
         return $this->render('main/creationlieu.html.twig', [
             'lieu' => $lieu,
@@ -206,7 +205,6 @@ class SortieController extends AbstractController
         } else {
 
         }
-
 
         return $this->render('main/creationville.html.twig', [
             'ville' => $ville,
