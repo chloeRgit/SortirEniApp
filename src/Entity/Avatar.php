432b3b43b1fca185cb1a16751a967033cf8d1d7c
chloeRgit <chloe.rondeau2020@campus-eni.fr>
2021-11-18 17:41:56 +0100

    avatar class

---
+++ src/Entity/Avatar.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AvatarRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AvatarRepository::class)
+ */
+class Avatar
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Participant::class, inversedBy="avatars")
+     */
+    private $participant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getParticipant(): ?Participant
+    {
+        return $this->participant;
+    }
+
+    public function setParticipant(?Participant $represent): self
+    {
+        $this->participant = $represent;
+
+        return $this;
+    }
+
+}
